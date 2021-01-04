8982a7aad0f4033fa8ec3df5e51abf895ddab86f
ggolubic <gabrijel.golubic@gmail.com>
2021-01-04 22:12:46 +0100

    this?

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
+        plugins: [`gatsby-remark-images`],
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-copy-linked-files`,
